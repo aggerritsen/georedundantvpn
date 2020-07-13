b5373d85a6b46da28ed58629018a83bbce567e06
Arjen Gerritsen <ag.gerritsen@emergoit.nl>
2020-07-13 15:06:42 +0200

    GEO

--- BuildDemo.ps1
+++ BuildDemo.ps1
@@ -1,7 +1,34 @@
 # BuildDemo.ps1
 
 . Deployment\01-Networks.ps1
-. Deployment\02-Gateways.ps1
+
+#. Deployment\02-Gateways.ps1
+$Process1 = start-process powershell -argument "Deployment\02a-GatewayHQ.ps1" -Passthru -NoNewWindow
+$Process2 = start-process powershell -argument "Deployment\02b-GatewayWest.ps1" -Passthru -NoNewWindow
+$Process3 = start-process powershell -argument "Deployment\02c-GatewayNorth.ps1" -Passthru -NoNewWindow
+
+while($True)
+{
+    Start-Sleep 60
+    $State = Get-Process -Id $Process1.Id -Erroraction SilentlyContinue
+    if(!$State)
+    {
+        "Gateway Deployment HQ Ended"
+        $State = Get-Process -Id $Process2.Id -Erroraction SilentlyContinue
+        if(!$State)
+        {
+            "Gateway Deployment West Ended"
+            $State = Get-Process -Id $Process3.Id -Erroraction SilentlyContinue
+            if(!$State)
+            {
+                "Gateway Deployment North Ended"
+                Break
+           }
+        }
+    }
+    "Gateway Deployment still running"
+}
+
 . Deployment\03-VNetPeering.ps1
 . Deployment\04-VPNPeering.ps1
 . Deployment\05-Machines.ps1
