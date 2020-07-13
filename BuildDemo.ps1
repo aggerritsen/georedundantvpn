f4a8581ce8a1913eaf07a49c5aad2f617a66fdb1
Arjen Gerritsen <ag.gerritsen@emergoit.nl>
2020-07-13 14:07:36 +0200

    GEO

---
+++ BuildDemo.ps1
@@ -0,0 +1,11 @@
+# BuildDemo.ps1
+
+. Deployment\01-Networks.ps1
+. Deployment\02-Gateways.ps1
+. Deployment\03-VNetPeering.ps1
+. Deployment\04-VPNPeering.ps1
+. Deployment\05-Machines.ps1
+. Deployment\06-Firewall.ps1
+. Deployment\07-FirewallRouting.ps1
+. Deployment\08-MonitoringV1.ps1
+. Deployment\09-MonitoringV2.ps1
